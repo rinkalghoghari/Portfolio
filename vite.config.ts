31547138d9257dd9de1f14ae0a584ba403008d40
Rinkal Ghoghari <rg.hmrtech@gmail.com>
2025-09-24 10:27:04 +0530

    update vite.config.json

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   root: path.resolve(__dirname, "client"),
   base: "./",
   build: {
-    outDir: "client/dist",
+    outDir: "dist" ,
     emptyOutDir: true,
   },
   server: {
